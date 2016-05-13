eab8e59889a09e1555e6b68f224e3eefc49d3af5
stskc <stskcr@gmail.com>
2016-05-13 19:31:29 +0900

    Delete below from config/routes.rb
    <<<<<<< HEAD
      get 'static_pages/home'
    =======
    =======
    >>>>>>> StaticPages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'static_pages/home'
-=======
 
   root 'static_pages#home'
   get 'static_pages/register'
->>>>>>> StaticPages
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
