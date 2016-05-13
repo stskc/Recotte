19f034d0819d569ceaebfe4cc68739c797d1bd89
stskc <stskcr@gmail.com>
2016-05-13 20:07:34 +0900

    Add iframe for youtube

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,16 @@
 module StaticPagesHelper
+
+  def movie(opts)
+    iframe = content_tag(
+      :iframe,
+      '', # empty body
+      width: 720,
+      height: 515,
+      src: "https://www.youtube.com/embed/#{opts[:movie_id]}",
+      frameborder: 0,
+      allowfullscreen: true
+    )
+    content_tag(:div, iframe, class: 'youtube-container')
+  end
+
 end
