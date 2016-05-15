9df2c47dbd5a0f65cb7f8dd9dc7a21535d9d43af
stskc <stskcr@gmail.com>
2016-05-15 19:42:58 +0900

    attempt twitter-oath

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -13,4 +13,24 @@ module StaticPagesHelper
     content_tag(:div, iframe, class: 'youtube-container')
   end
 
+=begin Amazon-associate
+
+# views に
+# <%= amazon_affiliate(affiliate_id: 'B00RTSCMXY') %>
+# ってかくと呼び出せる
+
+  def amazon_affiliate(opts)
+    iframe = content_tag(
+      :iframe,
+      '', # empty body
+      width: 320,
+      height: 390,
+      src: "http://rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=stskc-22&o=9&p=8&l=as1&m=amazon&f=ifr&ref=qf_sp_asin_til&asins=#{opts[:affiliate_id]}",
+      frameborder: 0,
+      allowfullscreen: true,
+    )
+    content_tag(:div, iframe, class: 'amazon-container')
+  end
+=end
+
 end
