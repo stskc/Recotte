1ada0fc72222c959bb485984f0149bca145f984b
stskc <stskcr@gmail.com>
2016-05-17 18:46:05 +0900

    Modify database

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @reco1 = Reco.where( 'id >= ?', rand(Reco.first.id..Reco.last.id) ).first
-    @reco2 = Reco.where( 'id >= ?', rand(Reco.first.id..Reco.last.id) ).last
+    @reco2 = Reco.where( 'id >= ?', rand(Reco.first.id..Reco.last.id) ).first
   end
 end
